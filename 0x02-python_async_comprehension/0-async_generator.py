aa22ec653f88bece2a37be5037c6b9ab3888b469
vpnchengo <charo.alvin@gmail.com>
2023-08-08 17:17:53 +0300

    update

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """async_generator corouting function"""
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator() -> float:
+async def async_generator() -> Generator[float, None, None]:
     """ generating a loop 10 times"""
     for _ in range(10):
         await asyncio.sleep(1)
