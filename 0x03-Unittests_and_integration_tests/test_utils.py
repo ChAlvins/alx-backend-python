7297b20213783eacb6588692e62ad97744746d5e
vpnchengo <charo.alvin@gmail.com>
2023-09-01 13:31:03 +0300

    update

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,7 +4,7 @@
 import unittest
 from typing import Dict, Tuple, Union
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 from unittest.mock import patch, Mock
 
 
